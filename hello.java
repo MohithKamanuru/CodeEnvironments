be08eeaee098e90b83179d1f28223fd116445e8b
dhruveel10 <66111178+dhruveel10@users.noreply.github.com>
2023-03-03 19:44:29 -0500

    Added data and resolved conflict

--- hello.java
+++ hello.java
@@ -1,5 +1,9 @@
 public class Hello{
 	public static void main(String args[]){
+		//Mohith pushed
 		System.out.println("Hello, my name is Mohith");
+		
+		//Dhruveel pushed
+		System.out.println("Hello, my name is Dhruveel");
 	}
 }
